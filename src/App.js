0913e0e435f0566c8b3c4e60fe96f1c7ffd4f7a3
Dharmil Shah <dharmilshah476@gmail.com>
2022-02-20 02:32:56 +0530

    Added components and added props and proptypes and various section of props and proptypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 // let myName = `Dharmil Shah`
 function App() {
@@ -7,29 +8,10 @@ function App() {
 
     // {} are used to implement javascript in between html
 
+    // Here We have added Navbar Component. Means whatever code we are writing related to navbar will be automatically effected here. In fact importing Navbar is also done automatically by just writing <Navbar/> here
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">About</a>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+    <Navbar title="This is title"/>
+    {/* <Navbar/> */}
     </>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+// Props are basically for understanding purpose we can say it is a parameter. In the App.js we pass this parameter using different names and it will render in the below code. We will be giving a particular format for accessing a particular element. For eg. we need to change the title, so we can add props.title in the curly braces. NOTE: props.title (here title is the name of the parameter that we'll be using in the App.js. We can name anything to this.)
+
+export default function Navbar(props) {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">{props.title}</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">About</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+// We can use propTypes for specifying the category to which the declration belongs.
+//Remember while initializing the propTypes, p should be small.
+
+Navbar.propTypes = {
+    title: PropTypes.string
+}
+
+// When we add isRequired at the end, means that field can't be empty or null.
+
+// Navbar.propTypes = {
+//     title: PropTypes.string.isRequired
+// }
+
+// We can set default props too...
+// When we have set the value of title in the App.js file then defaultProps value will not be considered. When the value is not set then defaultProps will be live.
+Navbar.defaultProps = {
+    title: `This Title`
+}
