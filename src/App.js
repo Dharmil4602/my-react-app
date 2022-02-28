4d09f201a5e92d7eb636b2deec7854a3116a3ee8
Dharmil Shah <dharmilshah476@gmail.com>
2022-02-28 12:13:54 +0530

    Created the switch in the navbar for dark mode enabling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import About from './Components/About';
+import React, {useState} from 'react';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
-// import TextForm from './Components/TextForm';
+import TextForm from './Components/TextForm';
 
 // let myName = `Dharmil Shah`
 function App() {
+  const [darkMode, setDarkMode] = useState(`light`); // States whether dark mode is enabled or not.
+
+  const toggleMode = () => {
+    if (darkMode === `light`) {
+      setDarkMode(`dark`);
+    }
+    else
+    {
+      setDarkMode(`light`);
+    }
+  }
+
   return (
     // Here we have added <> </>. These angular brackets are called JSX Fragments. Because in this angular brackets we can add as many html components we want. These are called JSX in which one or more html components are added.
 
@@ -12,11 +25,11 @@ function App() {
 
     // Here We have added Navbar Component. Means whatever code we are writing related to navbar will be automatically effected here. In fact importing Navbar is also done automatically by just writing <Navbar/> here
     <>
-    <Navbar title="This is title"/>
+    <Navbar title="This is title" mode={darkMode} toggleMode={toggleMode}/>
     {/* <Navbar/> */}
     <div className="container">
-     {/* <TextForm heading="Enter your message"/> */}
-     <About/>
+     <TextForm heading="Enter your message"/>
+     {/* <About/> */}
     </div>
     </>
   );
